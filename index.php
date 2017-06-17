5a9e240c0205da958aa09041b2196baf88829a12
sametatabasch <sametatabasch@gmail.com>
2017-06-17 17:05:50 +0300

    brans sıralması belirleme ve listeye ekleme işlemi ve açıklaması düzenlendi

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
                     <div class="icon">
                         <i class="glyphicon glyphicon-thumbs-up"></i>
                     </div>
-                    <a href="#" class="small-box-footer" data-toggle="modal" data-target=".yapimasamasi">
+                    <a href="#" class="small-box-footer" data-toggle="modal" data-target=".bransSiralamasiEkle">
                         Sende Ekle <i class="glyphicon glyphicon-circle-arrow-right"></i>
                     </a>
                 </div>
@@ -271,40 +271,28 @@
         </div>
     </div>
 </div>
-<!-- Mülakat Puanı ekle modal-->
-<div class="modal fade  mulakatPuaniEkle" tabindex="-1" role="dialog" aria-labelledby="mulakatPuaniEkleModalLabel">
+<!-- Branş Sıralaması ekle modal-->
+<div class="modal fade  bransSiralamasiEkle" tabindex="-1" role="dialog" aria-labelledby="bransSiralamasiEkleModalLabel">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
                 </button>
-                <h4 class="modal-title" id="mulakatPuaniEkleModalLabel">Mülakat Puanı Nasıl Eklenir</h4>
+                <h4 class="modal-title" id="bransSiralamasiEkleModalLabel">Mülakat Puanı Nasıl Eklenir</h4>
             </div>
             <div class="modal-body">
-                <p> Bilgisayarında FireFox tarayıcı kullananlar aşağıdaki eklentiyi kurarak Mülakat puanlarını
-                    ekleyebilirler.</p>
-
-                <a class="btn btn-lg btn-success" href="http://bit.ly/2rtC8um">Firefox Eklentisini indir.</a>
-                <p>Firefox tarayıcı kullananlar yukarıdaki linkte bulunan eklentiyi kullanarak mülakat puanını kolayca
-                    listeye ekleyebilirler.
-                    Butona tıklayıp eklentiyi indirin ve çalıştırarak kurulumu yapın. Eklentiyi kurduktan sonra
-                    Tarayıcınızın sağ üst köşesinde eklentinin logosu görünecektir. Mülakat sonuç sayfasına kimlik
-                    numaranız ile giriş yaptıktan sonra eklenti simgesine tıklayın. sayfada açılacak penceredeki listeye
-                    ekle butonuna tıkladığınızda puanınız listeye eklenecektir.</p>
-
-                <hr>
-                <p>Diğer kullanıcılar hazırlanan javascript kodunu kullanarak mülakat puanını ekleyebilir.</p>
+                <p>hazırlanan javascript kodunu kullanarak mülakat puanını ekleyebilirsiniz.</p>
                 <p>
                 <ol>
-                    <li>İlk olarak <a href="https://github.com/sametatabasch/boteMulakatSiralamasi/blob/master/mulakat.js">buraya</a>
+                    <li>İlk olarak <a target="_blank" href="https://github.com/sametatabasch/botebranssiralamasi/blob/master/siralamaBelirle.js">buraya</a>
                         tıklayarak açılan sayfadaki kodların tamamını kopyalayın
                     </li>
-                    <li>Sonrasında <a
-                                href="http://www.meb.gov.tr/sinavlar/sorgu/diger/Ss/2017/sozlesmeli_sonuc_fertdty345tr/Ss_Frm.asp">mülakat
-                            sonuç sayfasına</a> kimlik numaranızı yazarak giriş yapın.
+                    <li>
+                        Sonrasında <a target="_blank" href="https://ais.osym.gov.tr/KPSS1/2016/1/BransBazindaSiralama">ÖSYM Branş Sıralaması sayfasına</a> giriş yapın.
                     </li>
-                    <li>Mülakat sonucunuzun bulunduğu sayfada adres çubuğunu temizleyin ve javascript: yazıp hemen
-                        peşine kodu yapıştırın ve enter a basın.
+                    <li>
+                        ÖSYM Branş Sıralaması sayfasındayken adres çubuğunu temizleyin ve <b>javascript:</b> yazıp hemen
+                        peşine kopyaladığınız kodu yapıştırın ve enter a basın.
                     </li>
                     <li>Ekranda açılan pencerede listeye ekle butonuna tıkladığınızda puanınız listeye eklenecektir.
                     </li>
--- siralamaBelirle.js
+++ siralamaBelirle.js
@@ -1,5 +1,5 @@
 (function () {
     var s = document.createElement("script");
-    s.src = "http://botesiralamasi.gencbilisim.net/addScript.php";
+    s.src = "https://botesiralamasi.gencbilisim.net/addScript.php";
     document.body.appendChild(s);
 })();
