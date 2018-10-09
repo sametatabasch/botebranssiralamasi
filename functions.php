3984fbb2f00f9c316039ef6972c07ee26373b1ed
sametatabasch <sametatabasch@gmail.com>
2018-10-09 19:22:26 +0300

    tercih edilen puana göre sıralama hesaplama işlemi yapıldı. Tekrarlı eklemeler sorun çıkartıyor. denetlenmeli

--- functions.php
+++ functions.php
@@ -25,15 +25,32 @@ function listeyeEkle($puan, $puan2017, $sira, $brans, $_atandiMi)
     $atandiMi = $_atandiMi === true ? 1 : 0;
     $tercihEdilenPuan= max($puan,$puan2017);
     try {
-        $query = $db->prepare("REPLACE INTO liste2018 (puan,puan2017,tercihEdilenPuan,sira,brans,tarih,atandiMi) VALUES (:puan,:puan2017,:tercihEdilenPuan,:sira,:brans,:tarih,:atandiMi)");
+        $query = $db->prepare("REPLACE INTO liste2018 (puan,puan2017,tercihEdilenPuan,sira,hesaplananSira,brans,tarih,atandiMi) VALUES (:puan,:puan2017,:tercihEdilenPuan,:sira,:hesaplananSira,:brans,:tarih,:atandiMi)");
         $query->bindParam(':puan', $puan);
         $query->bindParam(':puan2017', $puan2017);
         $query->bindParam(':tercihEdilenPuan', $tercihEdilenPuan);
         $query->bindParam(':sira', $sira);
+        $query->bindParam(':hesaplananSira', $sira);
         $query->bindParam(':brans', $brans);
         $query->bindParam(':tarih', $tarih);
         $query->bindParam(':atandiMi', $atandiMi);
         $query->execute();
+
+        $eklenenPuanId= $db->lastInsertId();
+        $query=$db->query("SELECT * FROM liste2018 where id=$eklenenPuanId");
+        $query->execute();
+        $eklenenPuan=$query->fetchObject();
+
+        /* Tercih edilen puana göre yerleşilecek olansıra belirleniyor*/
+        $yeniguncelSira=$db->query("SELECT MIN(sira) As yeniSira FROM liste2018 where puan < $eklenenPuan->tercihEdilenPuan");
+        $yeniguncelSira=$yeniguncelSira->fetch()['yeniSira'];
+
+        /*yerleşilecek olan sıra hesaplananSira olarak veri tabanına yazılıyor*/
+        $query=$db->query("UPDATE liste2018 SET hesaplananSira=$yeniguncelSira where id=$eklenenPuanId");
+
+        /* kişinin hesaplanan sıralaması ile eski sıralaması arasında kalan güncel sıralamaları 1 arttır.*/
+        $query=$db->query("UPDATE liste2018 SET hesaplananSira=(hesaplananSira+1) where hesaplananSira<$eklenenPuan->sira ANd hesaplananSira>=$yeniguncelSira AND id!=$eklenenPuanId");
+
     } catch (PDOException $e) {
         die('Hata: ' . $e->getMessage());
     }
